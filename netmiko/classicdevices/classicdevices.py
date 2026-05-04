49677450ba6f6c7d2afe8d8f0e73bd30c5edf7ad
gviabcua <gviabcua@gmail.com>
2026-05-04 14:37:49 +0300

    Fix formatting with ruff

--- netmiko/classicdevices/classicdevices.py
+++ netmiko/classicdevices/classicdevices.py
@@ -47,9 +47,7 @@ class ClassicdevicesBase(CiscoBaseConnection):
         """Saves Config."""
         self.exit_config_mode()
         self.enable()
-        return super().save_config(
-            cmd=cmd, confirm=confirm, confirm_response=confirm_response
-        )
+        return super().save_config(cmd=cmd, confirm=confirm, confirm_response=confirm_response)
 
 
 class ClassicdevicesSSH(ClassicdevicesBase):
@@ -138,9 +136,9 @@ class ClassicdevicesTelnet(ClassicdevicesBase):
                     time.sleep(0.5 * delay_factor)
                     output = self.read_channel()
                     return_msg += output
-                    if re.search(
-                        pri_prompt_terminator, output, flags=re.M
-                    ) or re.search(alt_prompt_terminator, output, flags=re.M):
+                    if re.search(pri_prompt_terminator, output, flags=re.M) or re.search(
+                        alt_prompt_terminator, output, flags=re.M
+                    ):
                         return return_msg
 
                 # Check if proper data received
